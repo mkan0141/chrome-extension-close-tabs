c8f3dddf8532f74b6be15af0aba5ca268063d04b
mkan0141 <kanzume0141@gmail.com>
2025-03-09 11:51:07 +0900

    chore: setting app icon

--- manifest.ts
+++ manifest.ts
@@ -6,6 +6,11 @@ export const manifest = defineManifest({
   version: "0.0.1",
   manifest_version: 3,
   permissions: ["contextMenus", "tabs"],
+  icons: {
+    "16": "assets/icons/icon16.png",
+    "48": "assets/icons/icon48.png",
+    "128": "assets/icons/icon128.png",
+  },
   background: {
     service_worker: "src/service-worker.ts",
     type: "module",
